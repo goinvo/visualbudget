99ce5db75914d321c213ef91134d7b3f92cb62ae
hrothgar <hrothgarrrr@gmail.com>
2016-10-26 11:17:32 -0400

    skeleton of admin.js

---
+++ vis/vb-admin.js
@@ -0,0 +1,16 @@
+/**
+ * This file controls the visualization editor. It works on top of vb.js inside
+ * of the admin dashboard.
+ */
+
+(function($,d3,vb) {
+
+    var vbAdmin = (function(vba) {
+
+        vba.initialize = function() {
+            console.log('vbAdmin initialized.');
+        }
+
+    })(vbAdmin || {});
+
+})(jQuery, d3, vb);

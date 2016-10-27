1fd7dd96957c3790f3e0e3ed03f9c2eef1cafffe
hrothgar <hrothgarrrr@gmail.com>
2016-10-27 16:37:57 -0400

    JS module pattern is now good.

--- admin/js/vb-admin.js
+++ admin/js/vb-admin.js
@@ -3,14 +3,78 @@
  * of the admin dashboard.
  */
 
-(function($,d3,vb) {
 
-    var vbAdmin = (function(vba) {
+var visualbudget = (function(vb) {
 
-        vba.initialize = function() {
-            console.log('vbAdmin initialized.');
+    var admin = vb.admin = vb.admin || {};
+
+    admin.initialize = function() {
+        console.log('vbAdmin initialized.');
+        // console.log(vb);
+    }
+
+    return vb;
+
+})(visualbudget || {});
+
+
+
+/**
+ * Kick it off.
+ */
+
+
+/**
+ * Kick it off.
+ */
+(function(vb, $) {
+
+    $(document).ready(function () {
+        'use strict';
+        // vb.initialize();
+
+        String.prototype.format = function(placeholders) {
+            var s = this;
+            for(var propertyName in placeholders) {
+                var re = new RegExp('{' + propertyName + '}', 'gm');
+                s = s.replace(re, placeholders[propertyName]);
+            }
+            return s;
+        };
+
+        function setSelect() {
+            // Remove the existing chart by setting innerHTML to ''
+            var $chartDiv = $('#vb-chart');
+            $chartDiv.html('');
+
+            // Update the chart div data.
+            var $selectDiv = $("#vb-select-dataset option:selected");
+            $chartDiv.data($selectDiv.data());
         }
 
-    })(vbAdmin || {});
+        function generateShortcode() {
+            var $chartDiv = $('#vb-chart');
+            var $pre = $('#vb-shortcode');
+            var str = '[visualbudget data={dataset_id} vis={vistype}]';
+            $pre.html(str.format({
+                dataset_id: $chartDiv.data('vbDatasetId'),
+                vistype: $chartDiv.data('vbVisType')
+            }));
+        }
 
-})(jQuery, d3, vb);
+        $('#vb-select-dataset').change(function() {
+            console.log('Changing selected dataset.');
+            setSelect();
+            generateShortcode();
+            vb.initialize();
+        });
+
+        setSelect();
+        generateShortcode();
+
+        vb.initialize();
+        vb.admin.initialize();
+
+    });
+
+})(visualbudget, jQuery);
--- vis/vb.js
+++ vis/vb.js
@@ -2,217 +2,177 @@
  *  Visual Budget main routines.
  */
 
-(function($,d3) {
+
+
+/**
+ * Define vb.
+ */
+var visualbudget = (function (vb, $, d3) {
 
     /**
-     * Define vb.
+     * Initialize each chart.
      */
-    var vb = (function (v) {
+    vb.initialize = function() {
+        console.log('VB initializing...');
+        vb.charts = [];
+        var $chartDivs = $('.vb-chart');
+        $.when.apply($, $chartDivs.map(vb.tryToInitializeChart))
+            .then(vb.drawAllCharts);
+        // $chartDivs.each(vb.tryToInitializeChart)
+        //     .promise()
+        //     .done(vb.drawAllCharts);
+    }
 
-        /**
-         * Initialize each chart.
-         */
-        v.initialize = function() {
-            console.log('VB initializing...');
-            v.charts = [];
-            var $chartDivs = $('.vb-chart');
-            $.when.apply($, $chartDivs.map(v.tryToInitializeChart))
-                .then(v.drawAllCharts);
-            // $chartDivs.each(v.tryToInitializeChart)
-            //     .promise()
-            //     .done(v.drawAllCharts);
-        }
-
-        /**
-         * Try to initialize a chart. This function attempts to read the
-         * data from the given url. Upon success, the setupChartObject
-         * callback is invoked.
-         */
-        v.tryToInitializeChart = function() {
-            var $div = $(this);
-            var url = $div.data('vbDatasetUrl');
-            var jqXHR = $.getJSON(url)
-                .done(v.setupChartObject($div))
-                .fail(function( jqxhr, textStatus, error ) {
-                    var err = textStatus + ", " + error;
-                    console.log( "Request Failed: " + err );
-                });
-            return jqXHR;
-        }
-
-        /**
-         * Returns a callback to set up a new chart, with the jquery
-         * selector of the chart div element in scope.
-         */
-        v.setupChartObject = function($div) {
-            return function(data) {
-
-                var newChart = new v.Chart($div, data);
-                v.charts.push(newChart);
-                console.log('Added chart ' + $div.data('vbHash') + ' to queue.');
-            }
-        }
-
-        v.drawAllCharts = function() {
-            v.charts.forEach(function(chart, i, array) {
-                chart.draw();
+    /**
+     * Try to initialize a chart. This function attempts to read the
+     * data from the given url. Upon success, the setupChartObject
+     * callback is invoked.
+     */
+    vb.tryToInitializeChart = function() {
+        var $div = $(this);
+        var url = $div.data('vbDatasetUrl');
+        var jqXHR = $.getJSON(url)
+            .done(vb.setupChartObject($div))
+            .fail(function( jqxhr, textStatus, error ) {
+                var err = textStatus + ", " + error;
+                console.log( "Request Failed: " + err );
             });
-        }
-
-        return v;
-
-    })(vb || {});
-
+        return jqXHR;
+    }
 
     /**
-     * Augment for line chart.
+     * Returns a callback to set up a new chart, with the jquery
+     * selector of the chart div element in scope.
      */
-    var vb = (function (v) {
+    vb.setupChartObject = function($div) {
+        return function(data) {
 
-        v.Chart = function($div, data) {
-            this.$div = $div;
-            this.data = data;
-
-            var props = $div.data();
-            this.props = {};
-            this.props.datasetUrl = props.vbDatasetUrl;
-            this.props.datasetId = props.vbDatasetId;
-            this.props.visType = props.vbVisType;
-            this.props.hash = props.vbHash;
-        };
-
-
-        v.Chart.prototype.draw = function() {
-            console.log('Drawing chart ' + this.props.hash + '.');
-
-            switch(this.props.visType) {
-                case 'linechart':
-                    this.doLineChart();
-                    break;
-
-                default:
-                    this.$div.html('VisualBudget error: unrecognized chart type.');
-            }
+            var newChart = new vb.Chart($div, data);
+            vb.charts.push(newChart);
+            console.log('Added chart ' + $div.data('vbHash') + ' to queue.');
         }
+    }
+
+    vb.drawAllCharts = function() {
+        vb.charts.forEach(function(chart, i, array) {
+            chart.draw();
+        });
+    }
+
+    return vb;
+
+})(visualbudget || {}, jQuery, d3);
 
 
-        v.Chart.prototype.doLineChart = function() {
+/**
+ * Augment for line chart.
+ */
+var visualbudget = (function (vb) {
 
-            var data = this.data;
+    vb.Chart = function($div, data) {
+        this.$div = $div;
+        this.data = data;
 
-            // Set the dimensions of the canvas / graph
-            var margin = {top: 30, right: 20, bottom: 30, left: 50},
-                width = 600 - margin.left - margin.right,
-                height = 270 - margin.top - margin.bottom;
+        var props = $div.data();
+        this.props = {};
+        this.props.datasetUrl = props.vbDatasetUrl;
+        this.props.datasetId = props.vbDatasetId;
+        this.props.visType = props.vbVisType;
+        this.props.hash = props.vbHash;
+    };
 
-            // Parse the date / time
-            var parseDate = d3.timeFormat("%d-%b-%y").parse;
 
-            // Set the ranges
-            var x = d3.scaleTime().range([0, width]);
-            var y = d3.scaleLinear().range([height, 0]);
+    vb.Chart.prototype.draw = function() {
+        console.log('Drawing chart ' + this.props.hash + '.');
 
-            // Define the axes
-            // only show the year in the x-axis, not the month
-            var xAxis = d3.axisBottom().scale(x)
-                            .tickFormat(function(time, index) { return time.getUTCFullYear(); });
+        switch(this.props.visType) {
+            case 'linechart':
+                this.doLineChart();
+                break;
 
-            var yAxis = d3.axisLeft().scale(y)
-                            .ticks(5);
-
-            // Define the line
-            var valueline = d3.line()
-                .x(function(d) { return x(d.date); })
-                .y(function(d) { return y(d.dollarAmount); });
-
-            // Adds the svg canvas
-            var svg = d3.select('#' + this.$div.attr('id'))
-                .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                    .attr("transform",
-                          "translate(" + margin.left + "," + margin.top + ")");
-
-            data.dollarAmounts.forEach(function(d) {
-                d.date = Date.parse(d.date);
-                d.dollarAmount = +d.dollarAmount/1000;
-            });
-
-            // Scale the range of the data
-            x.domain(d3.extent(data.dollarAmounts, function(d) { return d.date; }));
-            y.domain([0, d3.max(data.dollarAmounts, function(d) { return d.dollarAmount; })]);
-
-            // Add the valueline path.
-            svg.append("path")
-                .attr("class", "line")
-                .attr("d", valueline(data.dollarAmounts));
-
-            // Add the X Axis
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            // Add the Y Axis
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis);
-
+            default:
+                this.$div.html('VisualBudget error: unrecognized chart type.');
         }
+    }
 
 
-        return v;
+    vb.Chart.prototype.doLineChart = function() {
 
-    })(vb || {});
+        var data = this.data;
 
+        // Set the dimensions of the canvas / graph
+        var margin = {top: 30, right: 20, bottom: 30, left: 50},
+            width = 600 - margin.left - margin.right,
+            height = 270 - margin.top - margin.bottom;
 
-    /**
-     * Kick it off.
-     */
-    $(document).ready(function () {
+        // Parse the date / time
+        var parseDate = d3.timeFormat("%d-%b-%y").parse;
 
-        String.prototype.format = function(placeholders) {
-            var s = this;
-            for(var propertyName in placeholders) {
-                var re = new RegExp('{' + propertyName + '}', 'gm');
-                s = s.replace(re, placeholders[propertyName]);
-            }
-            return s;
-        };
+        // Set the ranges
+        var x = d3.scaleTime().range([0, width]);
+        var y = d3.scaleLinear().range([height, 0]);
 
-        function setSelect() {
-            // Remove the existing chart by setting innerHTML to ''
-            var $chartDiv = $('#vb-chart');
-            $chartDiv.html('');
+        // Define the axes
+        // only show the year in the x-axis, not the month
+        var xAxis = d3.axisBottom().scale(x)
+                        .tickFormat(function(time, index) { return time.getUTCFullYear(); });
 
-            // Update the chart div data.
-            var $selectDiv = $("#vb-select-dataset option:selected");
-            $chartDiv.data($selectDiv.data());
-        }
+        var yAxis = d3.axisLeft().scale(y)
+                        .ticks(5);
 
-        function generateShortcode() {
-            var $chartDiv = $('#vb-chart');
-            var $pre = $('#vb-shortcode');
-            var str = '[visualbudget data={dataset_id} vis={vistype}]';
-            $pre.html(str.format({
-                dataset_id: $chartDiv.data('vbDatasetId'),
-                vistype: $chartDiv.data('vbVisType')
-            }));
-        }
+        // Define the line
+        var valueline = d3.line()
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.dollarAmount); });
 
-        $('#vb-select-dataset').change(function() {
-            console.log('Changing selected dataset.');
-            setSelect();
-            generateShortcode();
-            vb.initialize();
+        // Adds the svg canvas
+        var svg = d3.select('#' + this.$div.attr('id'))
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform",
+                      "translate(" + margin.left + "," + margin.top + ")");
+
+        data.dollarAmounts.forEach(function(d) {
+            d.date = Date.parse(d.date);
+            d.dollarAmount = +d.dollarAmount/1000;
         });
 
+        // Scale the range of the data
+        x.domain(d3.extent(data.dollarAmounts, function(d) { return d.date; }));
+        y.domain([0, d3.max(data.dollarAmounts, function(d) { return d.dollarAmount; })]);
+
+        // Add the valueline path.
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", valueline(data.dollarAmounts));
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+    }
+
+
+    return vb;
+
+})(visualbudget || {}, jQuery, d3);
+
+
+/**
+ * Kick it off.
+ */
+(function(vb, $) {
+    $(document).ready(function () {
         'use strict';
-        setSelect();
-        generateShortcode();
         vb.initialize();
-
     });
-
-})( jQuery, d3 );
+})(visualbudget, jQuery);

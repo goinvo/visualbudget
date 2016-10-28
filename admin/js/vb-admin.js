86701b36c4d7123b52d8e69c8ffe9f53f12e1497
hrothgar <hrothgarrrr@gmail.com>
2016-10-28 15:34:34 -0400

    Working with angular

--- admin/js/vb-admin.js
+++ admin/js/vb-admin.js
@@ -4,67 +4,64 @@
  */
 
 
-var visualbudget = (function(vb) {
-
-    var admin = vb.admin = vb.admin || {};
-
-    admin.initialize = function() {
-        console.log('vbAdmin initialized.');
-        // console.log(vb);
-    }
-
-    return vb;
-
-})(visualbudget || {});
-
-
 /**
  * Kick it off.
  */
 (function(vb, $) {
 
-    $(document).ready(function () {
-        'use strict';
-        // vb.initialize();
+    var vbAdmin = angular.module('vbAdmin', ['components']);
+    vbAdmin.controller('vbVisualizationsController', function($scope) {
+            $scope.datasets = [
+                {'uploaded_name': 'expenses.csv',
+                 'id': '18349273'},
+                {'uploaded_name': 'revenues.csv',
+                 'id': '22398172'}
+                ];
 
-        String.prototype.format = function(placeholders) {
-            var s = this;
-            for(var propertyName in placeholders) {
-                var re = new RegExp('{' + propertyName + '}', 'gm');
-                s = s.replace(re, placeholders[propertyName]);
-            }
-            return s;
-        };
-
-        function setSelect() {
-            // Remove the existing chart by setting innerHTML to ''
-            var $chartDiv = $('#vb-chart');
-            $chartDiv.html('');
-
-            // Update the chart div data.
-            var $selectDiv = $("#vb-select-dataset option:selected");
-            $chartDiv.data($selectDiv.data());
-        }
-
-        function generateShortcode() {
-            var $chartDiv = $('#vb-chart');
-            var $pre = $('#vb-shortcode');
-            var str = '[visualbudget data={dataset_id} vis={vistype}]';
-            $pre.html(str.format({
-                dataset_id: $chartDiv.data('vbDatasetId'),
-                vistype: $chartDiv.data('vbVisType')
-            }));
-        }
-
-        $('#vb-select-dataset').change(function() {
-            console.log('Changing selected dataset.');
-            setSelect();
-            generateShortcode();
-            vb.initialize();
+            $scope.vbDatasetSelect = $scope.datasets[0];
         });
 
-        setSelect();
-        generateShortcode();
+    $(document).ready(function () {
+        'use strict';
+
+        // String.prototype.format = function(placeholders) {
+        //     var s = this;
+        //     for(var propertyName in placeholders) {
+        //         var re = new RegExp('{' + propertyName + '}', 'gm');
+        //         s = s.replace(re, placeholders[propertyName]);
+        //     }
+        //     return s;
+        // };
+
+        // function setSelect() {
+        //     // Remove the existing chart by setting innerHTML to ''
+        //     var $chartDiv = $('#vb-chart');
+        //     $chartDiv.html('');
+
+        //     // Update the chart div data.
+        //     var $selectDiv = $("#vb-select-dataset option:selected");
+        //     $chartDiv.data($selectDiv.data());
+        // }
+
+        // function generateShortcode() {
+        //     var $chartDiv = $('#vb-chart');
+        //     var $pre = $('#vb-shortcode');
+        //     var str = '[visualbudget data={dataset_id} vis={vistype}]';
+        //     $pre.html(str.format({
+        //         dataset_id: $chartDiv.data('vbDatasetId'),
+        //         vistype: $chartDiv.data('vbVisType')
+        //     }));
+        // }
+
+        // $('#vb-select-dataset').change(function() {
+        //     console.log('Changing selected dataset.');
+        //     setSelect();
+        //     generateShortcode();
+        //     vb.initialize();
+        // });
+
+        // setSelect();
+        // generateShortcode();
 
         vb.initialize();
         vb.admin.initialize();

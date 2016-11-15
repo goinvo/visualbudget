73d7d17dce577ba41224f615e4561f970df1fa90
Hrothgar <hrothgar@maths.ox.ac.uk>
2016-11-15 11:24:52 -0500

    Trying to fix that call_user_func_array error.

--- public/class-visualbudget-public.php
+++ public/class-visualbudget-public.php
@@ -17,7 +17,7 @@ class VisualBudget_Public {
 	 */
 	public function enqueue_styles() {
 
-		wp_enqueue_style( 'vb_public_css', plugin_dir_url( __FILE__ ) . 'css/visualbudget-public.css', array(), $this->version, 'all' );
+		wp_enqueue_style( 'vb_public_css', plugin_dir_url( __FILE__ ) . 'css/visualbudget-public.css', array(), VISUALBUDGET_VERSION, 'all' );
 
         // Add the VB admin CSS file
         wp_enqueue_style( 'vis_css', plugin_dir_url( __FILE__ ) . '../vis/vb.css', array(), VISUALBUDGET_VERSION, 'all' );
@@ -31,7 +31,7 @@ class VisualBudget_Public {
         // Add D3
         wp_enqueue_script( 'd3_js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.6/d3.min.js', array(), VISUALBUDGET_VERSION, false );
 
-		wp_enqueue_script( 'vb_public_js', plugin_dir_url( __FILE__ ) . 'js/visualbudget-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( 'vb_public_js', plugin_dir_url( __FILE__ ) . 'js/visualbudget-public.js', array( 'jquery' ), VISUALBUDGET_VERSION, false );
 
         // Add the visualization js file and submodules.
         wp_enqueue_script( 'vb_js', plugin_dir_url( __FILE__ ) . '../vis/vb-all.js', array( 'jquery' ), VISUALBUDGET_VERSION, false );

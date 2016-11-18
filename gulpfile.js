4621de9c1a6f7f05ff3eca6dd165b8f16a463eaf
hrothgar <hrothgarrrr@gmail.com>
2016-11-18 17:56:48 -0500

    Reorganized directory structure once more.

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,31 @@ paths.visLib = paths.src + 'vis_lib/';
 paths.wpPlugin = paths.src + 'wp_plugin/';
 paths.wpPluginDist = paths.wpPlugin + 'plugin_dist/';
 
+// Edit this to be the path to the plugins folder
+// of your local WordPress install.
+paths.pluginExport = '/Users/hrothgar/Desktop/wordpress/wp-content/plugins/';
+
+/* ------------------------------------------------------------------------ */
+/*
+        Note to me:
+            The way that this will work is the following.
+
+            task 'vis-build'
+                - compiles js to dist
+                - compiles sass to dist
+
+            task 'plugin-build'
+                - copies PHP to dist
+                - compiles js to appropriate place in dist
+                - compiles sass to appropriate place in dist
+                - copies results of vis-build to dist (doesn't vis-build)
+
+            task 'plugin-move'
+                - moves plugin dist files to local path for testing
+
+            task 'build'
+                - does all of the above, in that order
+*/
 
 gulp.task('vis-compile-js', () => {
     return gulp.src(paths.visLib + 'js/**/*.js')
@@ -31,6 +56,13 @@ gulp.task('vis-compile-sass', () => {
     return gulp.src(paths.visLib + 'sass/vb.sass')
         .pipe(sass())
         .pipe(autoprefixer())
-})
+});
+
+
+
+gulp.task('plugin-copy', () => {
+    return gulp.src([paths.wpPluginDist + '**/*'])
+               .pipe(gulp.dest(paths.pluginExport + 'visualbudget/'));
+});
 
 gulp.task('vis', ['vis-compile-js']);

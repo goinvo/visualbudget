db9b98145e91ac20d92db59a43b927273e4448a2
hrothgar <hrothgarrrr@gmail.com>
2016-11-21 14:40:39 -0500

    iframe.php had incorrect paths to vis dependencies

--- plugin/src/php/vis/iframe.php
+++ plugin/src/php/vis/iframe.php
@@ -5,10 +5,10 @@ header("Content-type: text/html");
 ?><!DOCTYPE html>
 <html>
 <head>
-<link rel='stylesheet' type='text/css' href='vb.css'>
+<link rel='stylesheet' type='text/css' href='vb.min.css'>
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.6/d3.min.js'></script>
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.js'></script>
-<script type='text/javascript' src='vb-all.js'></script>
+<script type='text/javascript' src='vb.min.js'></script>
 </head>
 <body><?php
 echo $chart_element;

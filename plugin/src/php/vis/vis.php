7ac87e95ae4c7772d45f9addbff3b8feb266f1aa
Hrothgar <hrothgar@maths.ox.ac.uk>
2017-01-19 13:42:28 -0500

    Fixing another warning

--- plugin/src/php/vis/vis.php
+++ plugin/src/php/vis/vis.php
@@ -72,6 +72,11 @@ $id_or_alias_to_url = function($string) use ($get_aliases) {
 // If neither, set the URL anyway using the string provided (the user will
 // get an error). [FIXME: << is that right? should the chart just
 // say "invalid chart"?]
+
+// If there is no "data" variable, then it should be a "mytaxbill" vis.
+if(!isset($_GET['data'])) {
+    $_GET['data'] = '';
+}
 $dataset_names_array = $data_query_to_array($_GET['data']);
 $dataset_urls = array_map($id_or_alias_to_url, $dataset_names_array);
 if(count($dataset_urls) == 1) {
